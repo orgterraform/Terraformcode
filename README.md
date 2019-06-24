4a33333d9edc3bb6ea0400f66ea7f156a2b5114f
Ramesh <Ramesh.Akkiraju@gmail.com>
2019-06-24 21:21:38 +0100

    adding Env tfstatefiles

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-Terraorm code to create users for each environment
+Terraform code to create users for each environment 
 
 Step 1) clone the Git https://github.com/orgterraform/Terraformcode.git
-
 
 Step 2) Execute terraform init command
 
 Step 3) Execute the following commands
 
-        terraform workspace new dev
-        terraform workspace select dev
-        Terraform plan
-        Terraform Apply
-        
-Step 4) repeat step 3 for other environments test, qa, stage, prod        
+    terraform workspace select dev
+    terraform plan
+    terraform apply
+
+Step 4) repeat step 3 for other environments test, qa, stage, prod     
      
--- main.tf
+++ main.tf
@@ -5,52 +5,42 @@ provider "aws" {
 
 resource "aws_iam_user" "Nevsa" {
   name = "${var.iam_user1}-${terraform.workspace}"
-  path = "/"
 }
  
 resource "aws_iam_user" "Cordelia" {
   name = "${var.iam_user2}-${terraform.workspace}"
-  path = "/"
 }
 
 resource "aws_iam_user" "Kriste" {
   name = "${var.iam_user3}-${terraform.workspace}"
-  path = "/"
 }
  
 resource "aws_iam_user" "Darleen" {
   name = "${var.iam_user4}-${terraform.workspace}"
-  path = "/"
 }
 
 resource "aws_iam_user" "Wynnie" {
   name = "${var.iam_user5}-${terraform.workspace}"
-  path = "/"
 }
  
 resource "aws_iam_user" "Vonnie" {
   name = "${var.iam_user6}-${terraform.workspace}"
-  path = "/"
 }
 
 resource "aws_iam_user" "Emelita" {
   name = "${var.iam_user7}-${terraform.workspace}"
-  path = "/"
 }
  
 resource "aws_iam_user" "Maurita" {
   name = "${var.iam_user8}-${terraform.workspace}"
-  path = "/"
 }
 
 resource "aws_iam_user" "Devinne" {
   name = "${var.iam_user9}-${terraform.workspace}"
-  path = "/"
 }
  
 resource "aws_iam_user" "Breena" {
   name = "${var.iam_user10}-${terraform.workspace}"
-  path = "/"
 }
 
 
