6c4328ac56a3980de4b06f8e4f197577d6b23d6c
Ramesh <Ramesh.Akkiraju@gmail.com>
2019-06-24 14:56:52 +0100

    Terraform code

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-west-1"
+}
+
+resource "aws_iam_user" "Nevsa" {
+  name = "${var.iam_user1}-${terraform.workspace}"
+  path = "/"
+}
+ 
+resource "aws_iam_user" "Cordelia" {
+  name = "${var.iam_user2}-${terraform.workspace}"
+  path = "/"
+}
+
+resource "aws_iam_user" "Kriste" {
+  name = "${var.iam_user3}-${terraform.workspace}"
+  path = "/"
+}
+ 
+resource "aws_iam_user" "Darleen" {
+  name = "${var.iam_user4}-${terraform.workspace}"
+  path = "/"
+}
+
+resource "aws_iam_user" "Wynnie" {
+  name = "${var.iam_user5}-${terraform.workspace}"
+  path = "/"
+}
+ 
+resource "aws_iam_user" "Vonnie" {
+  name = "${var.iam_user6}-${terraform.workspace}"
+  path = "/"
+}
+
+resource "aws_iam_user" "Emelita" {
+  name = "${var.iam_user7}-${terraform.workspace}"
+  path = "/"
+}
+ 
+resource "aws_iam_user" "Maurita" {
+  name = "${var.iam_user8}-${terraform.workspace}"
+  path = "/"
+}
+
+resource "aws_iam_user" "Devinne" {
+  name = "${var.iam_user9}-${terraform.workspace}"
+  path = "/"
+}
+ 
+resource "aws_iam_user" "Breena" {
+  name = "${var.iam_user10}-${terraform.workspace}"
+  path = "/"
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "iam_user1" {
+ default     = "nevsa"
+ description = "User-Nevsa"
+}
+
+variable "iam_user2" {
+ default     = "cordelia"
+ description = "User-Cordelia"
+}
+
+variable "iam_user3" {
+ default     = "nekristevsa"
+ description = "User-Kriste"
+}
+
+variable "iam_user4" {
+ default     = "darleen"
+ description = "User-Darleen"
+}
+
+variable "iam_user5" {
+ default     = "wynnie"
+ description = "User-Wynnie"
+}
+
+variable "iam_user6" {
+ default     = "vonnie"
+ description = "User-Vonnie"
+}
+
+variable "iam_user7" {
+ default     = "emelita"
+ description = "User-Emelita"
+}
+
+variable "iam_user8" {
+ default     = "maurita"
+ description = "User-Maurita"
+}
+
+variable "iam_user9" {
+ default     = "devinne"
+ description = "User-Devinne"
+}
+
+variable "iam_user10" {
+ default     = "breena"
+ description = "User-Breena"
+}
+
